ff55ffee1e41d717f72d0f7ea676673cae1ac052
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 03:33:50 -0800

    Update story-array.php

--- story-array.php
+++ story-array.php
@@ -5,9 +5,15 @@ $story_array = [
 	// article׳
 	"what-is-al-nakba" => [
 
+		// article title
 		"title"	=> "What is Al-Nakba?",
 
-		// paragraphs
+		// article paragraphs
+		[
+		"heading"  => "Quick answer",
+		"paragraph"    => "...",
+		],
+	
 		[
 		"heading"  => "Quick answer",
 		"paragraph"    => "...",
