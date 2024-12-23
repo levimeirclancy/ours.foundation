03885a71c2ab1a86f406b663145a4f31470f2b0a
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 03:49:45 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
 
-echo "https://ours.foundation is a repository of knowledge by Levi Meir Clancy.";
+echo "<a href='https://database.ours.foundation'>database.ours.foundation</a> is a repository of knowledge by Levi Meir Clancy.";
 
 ?>
